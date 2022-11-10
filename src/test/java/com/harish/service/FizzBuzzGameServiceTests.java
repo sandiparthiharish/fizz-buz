6adbf148e570e9218f92d5e1163a9f1bdd2cb0a3
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-10 08:31:46 +0530

    refactor: Extract FizzBuzzGameSevice in tests and initialize before every test case using setup method

--- src/test/java/com/harish/service/FizzBuzzGameServiceTests.java
+++ src/test/java/com/harish/service/FizzBuzzGameServiceTests.java
@@ -1,5 +1,6 @@
 package com.harish.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -9,21 +10,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(MockitoJUnitRunner.class)
 public class FizzBuzzGameServiceTests {
 
+    private FizzBuzzGameService fizzBuzzGameService;
+
+    @Before
+    public void setUp() {
+        fizzBuzzGameService = new FizzBuzzGameService();
+    }
+
     @Test
     public void getFizzBuzzGameOutput_ReturnsNumberTest() {
-        FizzBuzzGameService fizzBuzzGameService = new FizzBuzzGameService();
         assertThat(fizzBuzzGameService.getFizzBuzzGameOutput(1)).isEqualTo("1");
     }
 
     @Test
     public void getFizzBuzzGameOutput_ReturnsFizzWithInput_3Test() {
-        FizzBuzzGameService fizzBuzzGameService = new FizzBuzzGameService();
         assertThat(fizzBuzzGameService.getFizzBuzzGameOutput(3)).isEqualTo("fizz");
     }
 
     @Test
     public void getFizzBuzzGameOutput_ReturnsFizzWithInputMultipleOf_3Test() {
-        FizzBuzzGameService fizzBuzzGameService = new FizzBuzzGameService();
         assertThat(fizzBuzzGameService.getFizzBuzzGameOutput(9)).isEqualTo("fizz");
     }
 }
