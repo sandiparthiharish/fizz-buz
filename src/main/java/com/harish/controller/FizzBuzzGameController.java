8b0aacf22a4fc66f3c5d5242decf20e65d5e056e
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-10 08:18:52 +0530

    refactor: Update FizzBuzzGameController class to use service class

--- src/main/java/com/harish/controller/FizzBuzzGameController.java
+++ src/main/java/com/harish/controller/FizzBuzzGameController.java
@@ -1,5 +1,6 @@
 package com.harish.controller;
 
+import com.harish.service.FizzBuzzGameService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FizzBuzzGameController {
 
+    private FizzBuzzGameService fizzBuzzGameService;
+
+    public FizzBuzzGameController(FizzBuzzGameService fizzBuzzGameService) {
+        this.fizzBuzzGameService = fizzBuzzGameService;
+    }
+
     @GetMapping(path = "/fizz-buzz/{number}")
     public String fizzBuzzGameHandler(@PathVariable int number) {
-        return String.valueOf(number);
+        return fizzBuzzGameService.getFizzBuzzGameOutput(number);
     }
 }
--- src/test/java/com/harish/controller/FizzBuzzGameControllerTests.java
+++ src/test/java/com/harish/controller/FizzBuzzGameControllerTests.java
@@ -1,12 +1,15 @@
 package com.harish.controller;
 
+import com.harish.service.FizzBuzzGameService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,16 +20,21 @@ public class FizzBuzzGameControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private FizzBuzzGameService fizzBuzzGameService;
+
     @Test
     public void fizzBuzzGameHandlerApi_NotFoundTest() throws Exception {
 
-        mockMvc.perform(get("/fizz-buzz/{number}", 1))
+        when(fizzBuzzGameService.getFizzBuzzGameOutput(1)).thenReturn("1");
+        mockMvc.perform(get("/fizz-buzz/{number}", ""))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     public void fizzBuzzGameHandlerApi_FoundTest() throws Exception {
 
+        when(fizzBuzzGameService.getFizzBuzzGameOutput(1)).thenReturn("1");
         mockMvc.perform(get("/fizz-buzz/{number}", 1))
                 .andExpect(status().isOk());
     }
